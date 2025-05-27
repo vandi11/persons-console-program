571aa028066eb9afdc01cdd2017a8d44ec6dcbd6
13asTerem <you@example.com>
2025-05-27 19:33:13 +0200

    c

--- obj/Debug/net8.0/PersonsConsoleProgram.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonsConsoleProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonsConsoleProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7afa5a2e3098f0dea752f28492cad8d7805a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonsConsoleProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonsConsoleProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
