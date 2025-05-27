db7afa5a2e3098f0dea752f28492cad8d7805a87
13asTerem <you@example.com>
2025-05-27 19:26:09 +0200

    project

---
+++ Models/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsConsoleProgram.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+         
+        public Person(string sor)
+        {
+            var adatok = sor.Split(";");
+            //Id = Convert.ToInt32(adatok[0]);
+            Name = adatok[1];
+            Age = Convert.ToInt32(adatok[2]);
+        }
+
+        public Person()
+        {
+        }
+
+        public override string? ToString()
+        {
+            return $"{Id};{Name};{Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using PersonsConsoleProgram.Models;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static public List<Person> Persons = new List<Person>();
+        static void AddPerson()
+        {
+            Console.Write("Név: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Kor");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Person p = new Person(name, age);
+            Persons.Add(p);
+
+        }
+
+        static void ListPersons()
+        {
+            foreach (Person p in Persons)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static void SaveToCsv()
+        {
+            File.WriteAllText("emberek.csv", string.Join("\n", Persons));
+        }
+
+        static void LoadFromCsv()
+        {
+            var sorok = File.ReadAllLines("emberek.csv");
+            foreach (var s in sorok)
+            {
+                Persons.Add(new Person(s));
+
+                //Console.WriteLine(s);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Person("Andi", 100);
+            Console.WriteLine(p);
+            LoadFromCsv();
+            AddPerson();
+           // AddPerson();
+           ListPersons();
+          // SaveToCsv();
+        }
+    }
+}
